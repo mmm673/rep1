4db62d624169e1c491462574bc73178997e85ccc
mmm673 <48325191+mmm673@users.noreply.github.com>
2019-03-08 18:57:10 +0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,10 @@
-#A = list(map(str, input().split()))
-
-A = [i+1 for i in range(т)]
-for i in range(1, len(A), 3):
-  A[i] = 'fizz'
-for i in range(1, len(A), 5):
-  if A[i] != 'fizz':
-    A[i] = 'buzz'
-  else:
-    A[i] = 'fizzbuzz'
-
-print(' '.join(map(str, A)))
+def fizzbuzz(n):
+  A = [i+1 for i in range(n)]
+  for i in range(1, len(A), 3):
+    A[i] = 'fizz'
+  for i in range(1, len(A), 5):
+    if A[i] != 'fizz':
+      A[i] = 'buzz'
+    else:
+      A[i] = 'fizzbuzz'
+  return A
