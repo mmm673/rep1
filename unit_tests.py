5e0afafa06615f53348af843ab4c2efad0da5a2f
mmm673 <48325191+mmm673@users.noreply.github.com>
2019-03-08 18:48:25 +0300

    Update unit_tests.py

--- unit_tests.py
+++ unit_tests.py
@@ -1,28 +1,12 @@
 #!/usr/bin/python3
 
 import unittest
-from calculator import cal
+from fizzbuzz import replace
 
-class TestCalculator(unittest.TestCase):
-    '''Testing the calculator'''
+class TestFizzbuzz(unittest.TestCase):
+  def test_fizzbuzz(self):
+    f = replace(5)
+    self.assertEqual(f, [1, 2, 'fizz', 4, 'buzz'])
 
-    def setUp(self):
-        '''Set up testing objects'''
-        self.a = 439
-        self.b = 4
-
-    def test_add(self):
-        '''Testing add menthod'''
-        calculator = cal(self.a, self.b)
-        # print(calculator.add())
-
-        self.assertEqual(calculator.add(),443)
-
-    def test_subtract(self):
-        '''Testing subtract method'''
-        calculator = cal(self.a, self.b)
-        self.assertEqual(calculator.sub(), 435)
-
-
-if __name__ == '__main__':
-    unittest.main(verbosity=2)
+if __name__==”__main__”:
+  unittest.main()
