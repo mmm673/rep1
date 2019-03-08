e2326372bf7f4b612170d894094097b3d25ad307
mmm673 <48325191+mmm673@users.noreply.github.com>
2019-03-08 16:10:06 +0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,6 @@
-A = list(map(str, input().split()))
+#A = list(map(str, input().split()))
 
+A = [i+1 for i in range(30)]
 for i in range(0, len(A), 3):
   A[i] = 'fizz'
 for i in range(0, len(A), 5):
