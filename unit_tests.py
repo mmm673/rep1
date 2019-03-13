5f7e4debd5e12a4feece2e89bced274d17d16d8c
mmm673 <48325191+mmm673@users.noreply.github.com>
2019-03-13 20:46:40 +0300

    Update unit_tests.py

--- unit_tests.py
+++ unit_tests.py
@@ -6,7 +6,7 @@ from fizzbuzz import replace
 class TestFizzbuzz(unittest.TestCase):
   def test_fizzbuzz(self):
     f = replace(5)
-    self.assertEqual(f, [1, 3, 'fizz', 4, 'buzz'])
+    self.assertEqual(f, [1, 2, 'fizz', 4, 'buzz'])
 
 if __name__=="__main__":
   unittest.main()
