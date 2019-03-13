ed9bcec7f43805a1ec097ea85f2d01c13ff8adfb
mmm673 <48325191+mmm673@users.noreply.github.com>
2019-03-13 19:58:49 +0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,8 @@
 def replace(n):
   A = [i+1 for i in range(n)]
-  for i in range(0, len(A), 3):
+  for i in range(2, len(A), 3):
     A[i] = 'fizz'
-  for i in range(0, len(A), 5):
+  for i in range(4, len(A), 5):
     if A[i] != 'fizz':
       A[i] = 'buzz'
     else:
