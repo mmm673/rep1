505b3e8590bb7c0485df1645012a1a682c4603df
mmm673 <48325191+mmm673@users.noreply.github.com>
2019-03-13 21:34:40 +0300

    Update unit_tests.py

--- unit_tests.py
+++ unit_tests.py
@@ -3,17 +3,20 @@
 import unittest
 from fizzbuzz import replace
 
-nums, A = [], []
-with open('tests.txt') as file:
-	for line in file:
-		nums.append(line[:line.find(' ')])
-		A.append(list(map(str, line[line.find(' ') + 1:].split())))
-
-class TestFizzbuzz(unittest.TestCase, nums, A):
-	def test_fizzbuzz(self, nums, A):
+class TestFizzbuzz(unittest.TestCase):
+	def get_tests():
+		nums, A = [], []
+		with open('tests.txt') as file:
+			for line in file:
+				nums.append(line[:line.find(' ')])
+				A.append(list(map(str, line[line.find(' ') + 1:].split())))
+		return nums, A
+	
+	def test_fizzbuzz(self):
+		nums, A = get_tests()
 		for i in range(len(nums)):
 			f = replace(nums[i])
 			self.assertEqual(f, A[i])
 
 if __name__=="__main__":
-	unittest.main(nums, A)
+	unittest.main()
