9c8d2449ef480f49694af0086a928302377bf9fb
mmm673 <48325191+mmm673@users.noreply.github.com>
2019-03-13 21:44:33 +0300

    Update unit_tests.py

--- unit_tests.py
+++ unit_tests.py
@@ -4,16 +4,12 @@ import unittest
 from fizzbuzz import replace
 
 class TestFizzbuzz(unittest.TestCase):
-	def get_tests():
+	def test_fizzbuzz(self):
 		nums, A = [], []
 		with open('tests.txt') as file:
 			for line in file:
 				nums.append(line[:line.find(' ')])
 				A.append(list(map(str, line[line.find(' ') + 1:].split())))
-		return nums, A
-	
-	def test_fizzbuzz(self):
-		nums, A = get_tests()
 		for i in range(len(nums)):
 			f = replace(nums[i])
 			self.assertEqual(f, A[i])
