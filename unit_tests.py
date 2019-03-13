54106137efafdc2eac85898a6d0e75b33062b9db
mmm673 <48325191+mmm673@users.noreply.github.com>
2019-03-13 21:27:01 +0300

    Update unit_tests.py

--- unit_tests.py
+++ unit_tests.py
@@ -9,7 +9,7 @@ with open('tests.txt') as file:
 		nums.append(line[:line.find(' ')])
 		A.append(list(map(str, line[line.find(' ') + 1:].split())))
 
-class TestFizzbuzz(unittest.TestCase):
+class TestFizzbuzz(unittest.TestCase, nums, A):
 	def test_fizzbuzz(self, nums, A):
 		for i in range(len(nums)):
 			f = replace(nums[i])
