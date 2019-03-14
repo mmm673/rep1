cb598eb50640497b9905aad21a254c5254bc45cb
mmm673 <48325191+mmm673@users.noreply.github.com>
2019-03-15 02:22:11 +0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,6 +5,7 @@ def replace(A):
       sum = 0
       for j in range(len(A[i])):
         sum += int(A[i][j])
+      A[i] = str(sum)
     if (A[i] == '0' or A[i] == '3' or A[i] == '6' or A[i] == '9') and (t[-1] == '0' or t[-1] == '5'):
       A[i] = 'fizzbuzz'
     elif (A[i] == '0' or A[i] == '3' or A[i] == '6' or A[i] == '9') and (t[-1] != '0' and t[-1] != '5'):
