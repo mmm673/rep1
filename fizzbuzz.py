f25b2335e3a542e951b6caeb8e24e40b92bba69d
mmm673 <48325191+mmm673@users.noreply.github.com>
2019-03-14 16:12:37 +0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,9 +3,11 @@ def replace(A):
     t = A[i]
     while A[i] >= 10:
       sum = 0
+      A[i] = str(A[i])
       for j in range(len(A[i])):
         sum += int(A[i][j])
-      A[i] = sum
+      A[i] = str(sum)
+    A[i] = int(A[i])
     if (A[i] == 0 or A[i] == 3 or A[i] == 6 or A[i] == 9) and (t[-1] == 0 or t[-1] == 5):
       A[i] = 'fizzbuzz'
     elif (A[i] == 0 or A[i] == 3 or A[i] == 6 or A[i] == 9) and (t[-1] != 0 and t[-1] != 5):
