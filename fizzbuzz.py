e33a80fc0b64f19e1db8e2bc25190301b3b978dd
mmm673 <48325191+mmm673@users.noreply.github.com>
2019-03-14 16:15:57 +0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,20 +3,18 @@ def replace(A):
     t = A[i]
     while A[i] >= 10:
       sum = 0
-      A[i] = str(A[i])
       for j in range(len(A[i])):
         sum += int(A[i][j])
-      A[i] = str(sum)
     A[i] = int(A[i])
-    if (A[i] == 0 or A[i] == 3 or A[i] == 6 or A[i] == 9) and (t[-1] == 0 or t[-1] == 5):
+    if (A[i] == '0' or A[i] == '3' or A[i] == '6' or A[i] == '9') and (t[-1] == '0' or t[-1] == '5'):
       A[i] = 'fizzbuzz'
-    elif (A[i] == 0 or A[i] == 3 or A[i] == 6 or A[i] == 9) and (t[-1] != 0 and t[-1] != 5):
+    elif (A[i] == '0' or A[i] == '3' or A[i] == '6' or A[i] == '9') and (t[-1] != '0' and t[-1] != '5'):
       A[i] = 'fizz'
-    elif (A[i] != 0 and A[i] != 3 and A[i] != 6 and A[i] != 9) and (t[-1] == 0 or t[-1] == 5):
+    elif (A[i] != '0' and A[i] != '3' and A[i] != '6' and A[i] != '9') and (t[-1] == '0' or t[-1] == '5'):
       A[i] = 'buzz'
     else:
       A[i] = t
   return A
 
-A = replace([1, 2, 3, 4])
+A = replace(['1', '2', '3', '4'])
 print(A)
